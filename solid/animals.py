cd7588577c431f0c6a01ad576472184faff3cfc3
vladimir-zlatev <125951708+vladimir-zlatev@users.noreply.github.com>
2023-07-14 16:01:50 -0700

    Added Ducks

--- solid/animals.py
+++ solid/animals.py
@@ -1,20 +1,32 @@
 class Animal:
-    def __init__(self, species):
-        self.species = species
+    def __init__(self, name):
+        self.name = name
 
-    def get_species(self):
-        return self.species
+    def make_sound(self):
+        return "Making sound"
+
+
+class Dog(Animal):
+    def make_sound(self):
+        return super().make_sound() + " wolf-wolf"
+
+
+class Cat(Animal):
+    def make_sound(self):
+        return super().make_sound() + " meow"
+
+
+class Turtle(Animal):
+    def make_sound(self):
+        return super().make_sound() + " T sound"
 
 
 def animal_sound(animals: list):
     for animal in animals:
-        if animal.species == 'cat':
-            print('meow')
-        elif animal.species == 'dog':
-            print('woof-woof')
+        print(animal.make_sound())
 
 
-animals = [Animal('cat'), Animal('dog')]
+animals = [Dog('Sharo'), Cat('Lady'), Turtle('nemo')]
 animal_sound(animals)
 
 ## добавете ново животно и рефакторирайте кода да работи без да се налага да се правят промени по него
